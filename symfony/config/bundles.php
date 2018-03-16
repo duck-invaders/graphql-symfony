fa9faa1bccc06385118c11aad75e2ccb664f05f0
Jonathan Jalouzot <jonathan.jalouzot@gmail.com>
2018-03-16 15:49:15 +0100

    feat: add type for graphql

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -6,4 +6,7 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
 ];
